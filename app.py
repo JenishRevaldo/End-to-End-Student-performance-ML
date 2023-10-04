6b6714cda68015c3f77aa0d27bd8948c717045ca
Jenish Revaldo <129158780+JenishRevaldo@users.noreply.github.com>
2023-10-04 12:46:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -55,4 +55,4 @@ def predict_datapoint():
         return render_template('result.html', results=predicted_math_score)
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0",port=8000)
